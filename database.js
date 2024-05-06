b4153055ccdd741fed68325dd63fb5206f7312a3
OGxE <alexchiarolini@gmail.com>
2024-05-06 10:19:16 +0200

    add

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
 console.log();
 const connpool = mysql.createPool({
-    host: "10.20.2.175",
+    host: "10.20.2.174",
     user: "tps",
     password: "ttppss",
     database: "negozioricambi"
